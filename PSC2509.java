34ddaf5a9c3783949320e9db3012631900fc467b
Lucas Arruda <105546865+LucasArruda-iwnl@users.noreply.github.com>
2023-09-25 21:22:30 -0300

    Add files via upload

---
+++ PSC2509.java
@@ -0,0 +1,8 @@
+public class PSC2509 {
+    public void exibirMensagem() {
+
+        System.out.println("Bem vindo ao livro de notas");
+
+    }
+
+}
---
+++ testelivro.java
@@ -0,0 +1,8 @@
+public class testelivro {
+    public static void main(String[] args) {
+
+        livroDeNotas Livro = new livroDeNotas();
+        Livro.exibirMensagem();
+    }
+
+}
